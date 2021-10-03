608f85abe2cdab6a30571fab63ce3204736c1617
G-Ragghianti <33492707+G-Ragghianti@users.noreply.github.com>
2021-10-03 19:26:29 -0400

    Create spack_setup.sh

---
+++ .github/CI/spack_setup.sh
@@ -0,0 +1,22 @@
+# This file should be "sourced" into your environment
+# to set up the spack repository
+
+DIR=/home/github/spack
+
+git clone https://github.com/spack/spack $DIR || true
+
+source $DIR/share/spack/setup-env.sh
+
+spack compiler find
+
+sload () {
+  # Installs a software package and loads it into the environment
+  spack install --fail-fast $@
+  spack load --first $@
+}
+
+slocation () {
+  # Returns the installation directory of a given software package
+  HASH=`spack find --no-groups --loaded -l $@ | head -1 | awk '{print $1}'`
+  spack location -i /$HASH
+}

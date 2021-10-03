3fc8a8ff1640d6c4469a2a46129bbf29b995fe29
Alan Ayala <aayala@icl.utk.edu>
2021-10-03 10:05:08 -0400

    FFTW C2C, C2R

--- include/fiber_backend_fftw.h
+++ include/fiber_backend_fftw.h
@@ -9,7 +9,7 @@
 #include <stdio.h>
 
 #if defined(FIBER_ENABLE_FFTW)
-#include "fftw3.h"
+#include <fftw3-mpi.h>
 
 //=====================  Complex-to-Complex transform =========================
 
@@ -18,33 +18,36 @@ void compute_z2z_fftw( int const inbox_low[3], int const inbox_high[3],
                   MPI_Comm const comm,
                   void const *in, void *out, double *timer)
 {
+    int niter = 1;
+    int nx = inbox_high[0]-inbox_low[0];
+    int ny = inbox_high[1]-inbox_low[1];
+    int nz = inbox_high[2]-inbox_low[2];
 
     // Plan creation ...
-    // void *plan;
+    void *plan_z2z;
+    int tuning_option = 0
 
     MPI_Barrier(comm);
     timer[0] = -MPI_Wtime();
-
-    // plan create
-
-    MPI_Barrier(comm);
+    if (tuning_option==0)
+        plan_z2z = fftw_mpi_plan_dft_c2c_3d(nx, ny, nz, in, out, comm, FFTW_ESTIMATE);
+    if (tuning_option==1)
+        plan_z2z = fftw_mpi_plan_dft_c2c_3d(nx, ny, nz, in, out, comm, FFTW_MEASURE);
     timer[0] += MPI_Wtime();
 
-
-
-    // FFT execution ...
+    // FFT execution
     MPI_Barrier(comm);
     timer[1] = -MPI_Wtime();
 
-    // compute FFT 
+	// for(int i = 0; i < niter; i++){
+		fftw_execute(plan_z2z);
+	// }
 
     MPI_Barrier(comm);
     timer[1] = +MPI_Wtime();
 
-    // Delete plan ...
-
-    printf("FFTW: Get Complex-to-Complex using the tester available in C. \n");
-    MPI_Abort(comm, 1);
+    // Delete plan
+	fftw_destroy_plan(plan_z2z);
 }
 
 //=====================  Real-to-Complex transform =========================
@@ -63,27 +66,29 @@ void compute_d2z_fftw( int const inbox_low[3], int const inbox_high[3],
 
     // Plan creation ...
     void *plan_d2z;
+    int tuning_option = 0
 
     MPI_Barrier(comm);
     timer[0] = -MPI_Wtime();
-    plan_d2z = fftw_plan_dft_3d(nx, ny, nz, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+    if (tuning_option==0)
+        plan_d2z = fftw_mpi_plan_dft_r2c_3d(nx, ny, nz, in, out, comm, FFTW_ESTIMATE);
+    if (tuning_option==1)
+        plan_d2z = fftw_mpi_plan_dft_r2c_3d(nx, ny, nz, in, out, comm, FFTW_MEASURE);
     timer[0] += MPI_Wtime();
-
 
     // FFT execution
     MPI_Barrier(comm);
     timer[1] = -MPI_Wtime();
 
-	for(int i = 0; i < niter; i++){
+	// for(int i = 0; i < niter; i++){
 		fftw_execute(plan_d2z);
-	}
+	// }
 
     MPI_Barrier(comm);
     timer[1] = +MPI_Wtime();
 
     // Delete plan
 	fftw_destroy_plan(plan_d2z);
-    printf("FFTW: Get Real-to-Complex using the tester available in C. \n");
 }
 
 //=====================  Complex-to-Real transform =========================
@@ -94,7 +99,36 @@ void compute_z2d_fftw( int const inbox_low[3], int const inbox_high[3],
                   MPI_Comm const comm,
                   void const *in, double *out, double *timer)
 {
-// Missing!
+    int niter = 1;
+    int nx = inbox_high[0]-inbox_low[0];
+    int ny = inbox_high[1]-inbox_low[1];
+    int nz = inbox_high[2]-inbox_low[2];
+
+    // Plan creation ...
+    void *plan_z2d;
+    int tuning_option = 0
+
+    MPI_Barrier(comm);
+    timer[0] = -MPI_Wtime();
+    if (tuning_option==0)
+        plan_z2d = fftw_mpi_plan_dft_c2r_3d(nx, ny, nz, in, out, comm, FFTW_ESTIMATE);
+    if (tuning_option==1)
+        plan_z2d = fftw_mpi_plan_dft_c2r_3d(nx, ny, nz, in, out, comm, FFTW_MEASURE);
+    timer[0] += MPI_Wtime();
+
+    // FFT execution
+    MPI_Barrier(comm);
+    timer[1] = -MPI_Wtime();
+
+	// for(int i = 0; i < niter; i++){
+		fftw_execute(plan_z2d);
+	// }
+
+    MPI_Barrier(comm);
+    timer[1] = +MPI_Wtime();
+
+    // Delete plan
+	fftw_destroy_plan(plan_z2d);
 
 }
 

7a88038cb1ed442cf36306db1a16b396f8a34b54
Alan Ayala <aayala@icl.utk.edu>
2021-10-24 17:53:11 -0400

    NB3DFFT multiple plan options

--- include/fiber_backend_nb3dfft.h
+++ include/fiber_backend_nb3dfft.h
@@ -15,7 +15,7 @@
 void compute_z2z_nb3dfft( int const inbox_low[3], int const inbox_high[3],
                   int const outbox_low[3], int const outbox_high[3], 
                   MPI_Comm const comm,
-                  void const *in, void *out, int nb3dfft_switch, double *timer)
+                  void const *in, void *out, int *nb3dfft_options, double *timer)
 {
     printf("Complex-to-Complex transform is not available for nb3dFFT. \n");
     MPI_Abort(comm, 1);
@@ -29,7 +29,7 @@ void compute_z2z_nb3dfft( int const inbox_low[3], int const inbox_high[3],
 void compute_d2z_nb3dfft( int const inbox_low[3], int const inbox_high[3],
                   int const outbox_low[3], int const outbox_high[3], 
                   MPI_Comm const comm,
-                  double const *in, void *out, int nb3dfft_switch, double *timer)
+                  double const *in, void *out, int *nb3dfft_options, double *timer)
 {
 
     // Plan creation ...
@@ -65,7 +65,7 @@ void compute_d2z_nb3dfft( int const inbox_low[3], int const inbox_high[3],
 void compute_z2d_nb3dfft( int const inbox_low[3], int const inbox_high[3],
                   int const outbox_low[3], int const outbox_high[3], 
                   MPI_Comm const comm,
-                  void const *in, double *out, int nb3dfft_switch, double *timer)
+                  void const *in, double *out, int *nb3dfft_options, double *timer)
 {
 // Missing!
 
@@ -75,19 +75,19 @@ void compute_z2d_nb3dfft( int const inbox_low[3], int const inbox_high[3],
 void compute_z2z_nb3dfft( int const inbox_low[3], int const inbox_high[3],
                   int const outbox_low[3], int const outbox_high[3], 
                   MPI_Comm const comm,
-                  void const *in, void *out, int nb3dfft_switch, double *timer)
+                  void const *in, void *out, int *nb3dfft_options, double *timer)
 {}
 
 void compute_d2z_nb3dfft( int const inbox_low[3], int const inbox_high[3],
                   int const outbox_low[3], int const outbox_high[3], 
                   MPI_Comm const comm,
-                  double const *in, void *out, int nb3dfft_switch, double *timer)
+                  double const *in, void *out, int *nb3dfft_options, double *timer)
 {}
 
 void compute_z2d_nb3dfft( int const inbox_low[3], int const inbox_high[3],
                   int const outbox_low[3], int const outbox_high[3], 
                   MPI_Comm const comm,
-                  void const *in, double *out, int nb3dfft_switch, double *timer)
+                  void const *in, double *out, int *nb3dfft_options, double *timer)
 {}
 
 #endif 

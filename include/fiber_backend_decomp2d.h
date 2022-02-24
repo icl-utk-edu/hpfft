d061dde10affecd8b687f9da48c2881c9d1dfa80
Sebastien Cayrols <sebastien.cayrols@gmail.com>
2022-02-24 17:04:13 -0500

    Add the 2decomp&FFT backend

--- include/fiber_backend_decomp2d.h
+++ include/fiber_backend_decomp2d.h
@@ -13,10 +13,15 @@
 
 //=================== Initialization (if required) ============================
 int init_decomp2d(int physical, int nx, int ny, int nz, int p_row, int p_col){
+    // plan create
+    decomp_2d_init( nx, ny, nz*2, p_row, p_col );
+    decomp_2d_fft_init( physical );
     return(0);
 }
 
 int finalize_decomp2d(){
+    // Delete plan ...
+    decomp_2d_finalize();
     return(0);
 }
 
@@ -26,33 +31,34 @@ void compute_z2z_decomp2d( int const inbox_low[3], int const inbox_high[3],
                   MPI_Comm const comm,
                   void const *in, void *out, int *decomp2d_options, double *timer)
 {
-
     // Plan creation ...
-    // void *plan;
+    int physical  = PHYSICAL_IN_X; // XXX has to be the same as the init phase
+    int lxsize    = 0;
+    int lysize    = 0;
+    int lzsize    = 0;
+    int direction = 0;
+
+    direction = ( decomp2d_options[0] == 0 ? FORWARD : BACKWARD );
 
     MPI_Barrier(comm);
     timer[0] = -MPI_Wtime();
 
-    // plan create
+    decomp_2d_get_local_sizes( physical, &lxsize, &lysize, &lzsize );
 
     MPI_Barrier(comm);
     timer[0] += MPI_Wtime();
-
-
 
     // FFT execution ...
     MPI_Barrier(comm);
     timer[1] = -MPI_Wtime();
 
     // compute FFT 
+    decomp_2d_fft_3d_c2c( lxsize, lysize, lzsize, (double complex*)in, 
+        lxsize, lysize, lzsize, (double complex*)out, direction );
 
     MPI_Barrier(comm);
     timer[1] = +MPI_Wtime();
 
-    // Delete plan ...
-
-    printf("DECOM2D: Get Complex-to-Complex using a wrapper to functions in fft_test_c2c folder \n");
-    MPI_Abort(comm, 1);
 }
 
 //=====================  Real-to-Complex transform =========================
@@ -63,6 +69,8 @@ void compute_d2z_decomp2d( int const inbox_low[3], int const inbox_high[3],
                   MPI_Comm const comm,
                   double const *in, void *out, int *decomp2d_options, double *timer)
 {
+    printf( "SKIP %s\n", __FUNCTION__ );
+    return ;
 
     // Plan creation ...
     // void *plan;

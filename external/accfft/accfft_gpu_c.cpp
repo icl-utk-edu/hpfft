f5114c52322f1beec38277da488de3e4eeff8346
af-ayala <aayala@icl.utk.edu>
2022-04-04 01:09:23 -0400

    AccFFT C-interface for GPU and CPU

---
+++ external/accfft/accfft_gpu_c.cpp
@@ -0,0 +1,48 @@
+/* ----------------------------------------------------------------------
+    C interface to AccFFT library, 3-D FFT functions for GPU hardware
+    Author: Alan Ayala, aayala@icl.utk.edu
+
+    Add this file into accfft/include folder.
+    Allows calling AccFFT from a C program.
+------------------------------------------------------------------------- */
+
+#include <mpi.h>
+#include <stdio.h>
+
+#include "accfft_gpu.h"
+#include "accfft_c.h"
+
+#include <cuda_runtime_api.h>
+
+
+/* ----------------------------------------------------------------------
+    Create AccFFT plan GPU-case
+------------------------------------------------------------------------- */
+
+void accfft_create_plan_gpu(int * n, Complex * data, Complex * data_out,
+		MPI_Comm c_comm, unsigned flags, void ** plan)
+{
+    accfft_plan_gpu * ptr = accfft_plan_dft_3d_c2c_gpu(n, data, data_out, c_comm, ACCFFT_MEASURE);
+    *plan = (void *) ptr;
+}
+
+/* ----------------------------------------------------------------------
+    Execute AccFFT plan GPU-case
+------------------------------------------------------------------------- */
+
+void accfft_compute_gpu(void * ptr, Complex * data, Complex * data_out, int flag)
+{
+    accfft_plan_gpu * plan = (accfft_plan_gpu *) ptr;
+    accfft_execute_c2c_gpu(plan, flag, data, data_out);
+}
+
+
+/* ----------------------------------------------------------------------
+    Delete plan GPU-case
+------------------------------------------------------------------------- */
+
+void accfft_destroy_plan_gpu_c(void * ptr)
+{
+    accfft_plan_gpu *plan = (accfft_plan_gpu *) ptr;
+	accfft_destroy_plan_gpu(plan);
+}

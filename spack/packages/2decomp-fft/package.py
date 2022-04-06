7a964035eb8e224d3dcf6b8e6853332506fc4534
Gerald Ragghianti <ragghianti@icl.utk.edu>
2022-04-06 15:44:35 -0400

    Improved 2decomp package features

--- spack/packages/2decomp-fft/package.py
+++ spack/packages/2decomp-fft/package.py
@@ -20,15 +20,46 @@ algorithms."""
 
     version('1.5.847', sha256='b137d7cf9b771de0a174d1e6c4ff0e48b4a84b51ff6c62140b4f522092e6784f')
 
-    depends_on('mpi')
+    variant(
+        'backend', default='generic', description='FFT backend',
+        values=('generic', 'fftw3', 'mkl'), multi=False
+    )
 
-    @property
-    def build_targets(self):
-        return ['-j1']
+    depends_on('mpi')
+    depends_on('mkl', when='backend=mkl')
+    depends_on('fftw', when='backend=fftw3')
+
+    #@property
+    #def build_targets(self):
+    #    return ['-j1']
 
     def edit(self, spec, prefix):
-        with working_dir('src'):
-            copy('Makefile.inc.x86', 'Makefile.inc')
+        makefile = FileFilter('src/Makefile')
+        makefile.filter('include Makefile.inc', '')
+        #with working_dir('src'):
+        #    touch('Makefile.inc')
+
+    def build(self, spec, prefix):
+        make_args = ['-j1', # This avoids a race condition in the Makefile
+                     'FFT={0}'.format(spec.variants['backend'].value),
+                     'LDFLAGS=-O3',
+                     'CC=mpicc',
+                     'F90=mpif90',
+                     'IFORT=gfortran',
+                     'OPTIONS=-DDOUBLE_PREC'
+                    ]
+        f90flags = 'F90FLAGS=-O3 -fcray-pointer -cpp'
+        if 'backend=fftw3' in spec:
+            f90flags += ' -I{0}'.format(spec['fftw'].prefix.include)
+        if 'backend=mkl' in spec:
+            f90flags += ' -I{0}'.format(spec['mkl'].prefix.include)
+            make_args.append('MKL_PATH={0}/mkl'.format(spec['mkl'].prefix))
+        make_args.append(f90flags)
+        make('lib', *make_args)
 
     def install(self, spec, prefix):
-        make('install', 'prefix={0}'.format(prefix))
+        # Avoid using the default "make install" because it
+        # requires building the example codes.
+        mkdirp(prefix.include, prefix.lib)
+        install('include/*.mod', prefix.include)
+        install('lib/lib*.a', prefix.lib)

d6385c13737558da463b19f63f3581f301f5e731
G-Ragghianti <ragghianti@icl.utk.edu>
2023-02-20 15:44:57 -0500

    CI fix

---
+++ .github/workflows/build_spack.sh
@@ -0,0 +1,24 @@
+#!/bin/bash --login
+
+MPI=$1
+FFT=$2
+
+source /etc/profile
+set +x
+set -e
+trap 'echo "# $BASH_COMMAND"' DEBUG
+shopt -s expand_aliases
+
+export HOME=`pwd`
+git clone https://github.com/spack/spack spack_upstream || true
+source spack_upstream/share/spack/setup-env.sh
+module load gcc@7.3.0
+spack compiler find
+spack repo add `pwd`/spack/ || true
+spack uninstall -a -y --dependents $FFT || true
+spack uninstall -a -y fiber || true
+spack dev-build fiber fft=$FFT
+
+# Run the tests
+spack load fiber
+mpirun -n 2 test3D_C2C -lib $FFT -backend fftw -size 4 4 4 -pgrid 1 2
